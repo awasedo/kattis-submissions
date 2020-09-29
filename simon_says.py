7ec41ffa7ea2d47b4c2fa7b3813316faccc0b7a4
Eskil Darpe <eskil@darpe.se>
2020-09-29 21:58:54 +0200

    Shortened Simon says solution
    From 0.8 to 0.7 seconds!

--- simon_says.py
+++ simon_says.py
@@ -1,9 +1,4 @@
-inps = []
 for i in range(int(input())):
-	inp = list(map(str, input().split()))
-	if inp[0] == ("Simon") and inp[1] == ("says"):
-		inp.remove(inp[0])
-		inp.remove(inp[0])
-		inps.extend(inp)
-		inps.append("\n")
-print(" ".join(inps))
+    inp = input()
+    if "Simon says" in inp:
+        print(inp[11:])
